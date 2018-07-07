f10d0a5bb5132a14469ab268c3ddeec25dddaadf
fiveyuan <fiveyuan05@163.com>
2018-07-07 12:32:30 +0800

    004 Median of Two Sorted Arrays

---
+++ CSharp/LeetCode/004/Median of Two Sorted Arrays.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode._004
+{
+    public class Median_of_Two_Sorted_Arrays
+    {
+        public double FindMedianSortedArrays(int[] nums1, int[] nums2)
+        {
+            var list = new List<int>();
+            int i = 0, j = 0;
+            while (true)
+            {
+                if (i >= nums1.Length && j >= nums2.Length)
+                {
+                    break;
+                }
+                else if (i < nums1.Length && j < nums2.Length)
+                {
+                    if (nums1[i] < nums2[j])
+                    {
+                        list.Add(nums1[i]);
+                        i++;
+
+                    }
+                    else
+                    {
+                        list.Add(nums2[j]);
+                        j++;
+                    }
+                }
+                else if (i >= nums1.Length)
+                {
+                    list.Add(nums2[j]);
+                    j++;
+                }
+                else if (j >= nums2.Length)
+                {
+                    list.Add(nums1[i]);
+                    i++;
+                }
+
+            }
+            if (list.Count == 0)
+            {
+                return 0;
+            }
+            if (list.Count % 2 == 0)
+            {
+                return (list[list.Count / 2] + list[(list.Count / 2) - 1]) / 2.0;
+            }
+            else
+            {
+                return list[(list.Count / 2)];
+            }
+
+        }
+        public double FindMedianSortedArraysFaster(int[] A, int[] B)
+        {
+
+            int m = A.Length;
+            int n = B.Length;
+            if (m > n)
+            { // to ensure m<=n
+                int[] temp = A; A = B; B = temp;
+                int tmp = m; m = n; n = tmp;
+            }
+            int iMin = 0, iMax = m, halfLen = (m + n + 1) / 2;
+            while (iMin <= iMax)
+            {
+                int i = (iMin + iMax) / 2;
+                int j = halfLen - i;
+                if (i < iMax && B[j - 1] > A[i])
+                {
+                    iMin = iMin + 1; // i is too small
+                }
+                else if (i > iMin && A[i - 1] > B[j])
+                {
+                    iMax = iMax - 1; // i is too big
+                }
+                else
+                { // i is perfect
+                    int maxLeft = 0;
+                    if (i == 0) { maxLeft = B[j - 1]; }
+                    else if (j == 0) { maxLeft = A[i - 1]; }
+                    else { maxLeft = Math.Max(A[i - 1], B[j - 1]); }
+                    if ((m + n) % 2 == 1) { return maxLeft; }
+
+                    int minRight = 0;
+                    if (i == m) { minRight = B[j]; }
+                    else if (j == n) { minRight = A[i]; }
+                    else { minRight = Math.Min(B[j], A[i]); }
+
+                    return (maxLeft + minRight) / 2.0;
+                }
+            }
+            return 0.0;
+
+
+            return 0;
+        }
+
+    }
+}
--- CSharp/LeetCode/Program.cs
+++ CSharp/LeetCode/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 
 namespace LeetCode
 {
@@ -9,20 +10,31 @@ namespace LeetCode
     {
         static void Main(string[] args)
         {
-            var list = new List<char>();
-            list.Add('a'); 
-            list.Add('b');
-            list.Add('b');
-            list.Add('b');
-            list.Add('a');
-            list.Add('a');
-            list.Add('a');
+            //var test = new _004.Median_of_Two_Sorted_Arrays();
+            //var re = test.FindMedianSortedArrays(new int[] { 1, 2 }, new int[] { 3, 4 });
 
-            var index = list.LastIndexOf('a',2);
+            //int[] a = { 1, 2, 3, 4, 5, 6, 7, 9, 1, 2, 3, 4, 5, 6, 7, 9, 12 };
+            //int r = a[0];
+            //for (int i = 1; i < a.Length; i++)
+            //{
+            //    r = r ^ a[i];
+            //}
+            //Console.WriteLine(r.ToString());
+            testobj t = new testobj();
+            t.s = "new";
+            add(t);
 
-            var test = new _003.Longest_Substring_Without_Repeating_Characters();
-            var re = test.LengthOfLongestSubstringList("abcabcbb");
+            Console.WriteLine(t.s);
 
+            Console.ReadLine();
+        }
+        public static void add(testobj t)
+        {
+            t.s = t.s + "add";
+        }
+        public class testobj
+        {
+            public string s;
         }
 
     }
