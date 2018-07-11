782bdd5affc8f3b5a6ff7240df699156d6c4bb16
fiveyuan <fiveyuan05@163.com>
2018-07-11 20:32:55 +0800

    005

---
+++ CSharp/LeetCode/005/Longest Palindromic Substring.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode._005
+{
+    public class Longest_Palindromic_Substring
+    {
+        public string LongestPalindrome(string s)
+        {
+            int m, n, x, y;
+            int rl = 1;
+
+            int l = s.Length;
+            if (s.Length <= 1)
+            {
+                return s;
+            }
+            var sArray = s.ToCharArray();
+            x = 0;
+            while (true)
+            {
+
+                //start 
+                y = x;
+
+                //find same item
+                while (true)
+                {
+                    if (sArray[x] != sArray[++y])
+                    {
+                        y--;
+                        break;
+                    }
+                }
+
+                //follow two side
+                for (int i = 1; i < x; i++)
+                {
+                    if (y + i <= l)
+                    {
+                        if (sArray[x - i] == sArray[y + i])
+                        {
+                            m = x - i;
+                            n = y + i;
+                            continue;
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+
+                // update m n
+
+
+            }
+
+
+            return "";
+        }
+    }
+}
